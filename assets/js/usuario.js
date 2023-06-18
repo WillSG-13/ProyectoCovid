0db4c50898f9bf3ffaacea92aade469178e1d54c
WillSG-13 <Wilmarsequeira13@gmail.com>
2023-06-17 21:49:41 -0600

    mandando

--- assets/js/usuario.js
+++ assets/js/usuario.js
@@ -1,60 +1,50 @@
-var idUsuario = 0;
-var numeroCedula = 0;
-var nombreCompleto = "";
-var fechaNacimiento = "";
-var numeroTelefono = 0;
-var email = "";
-var contrasena = "";
-var rol = "";
-var estado = "";
-
-
+var tablaUsuario = document.querySelector('#tBody-Usuario');
 function cargarDatos() {
-    numeroCedula = parseInt(document.getElementById('numeroCedula').value);
-    nombreCompleto = document.getElementById('nombreCompleto').value;
-    fechaNacimiento = document.getElementById('fechaNacimiento').value;
-    numeroTelefono = parseInt(document.getElementById('numeroTelefono').value);
-    email = document.getElementById('email').value;
-    contrasena = document.getElementById('password').value;
-    rol = document.getElementById('rol').value;
-    estado = "Act";
-    idUsuario = 0;
-}
-cargarUsuario();
-var frm = document.getElementById("frmUsuario");
-frm.addEventListener('submit',
-    function (e) {
-        e.preventDefault();
-        cargarDatos();
-        var data ={
+   var numeroCedula = parseInt(document.getElementById('numeroCedula').value);
+   var nombreCompleto = document.getElementById('nombreCompleto').value;
+   var fechaNacimiento = document.getElementById('fechaNacimiento').value;
+   var numeroTelefono = parseInt(document.getElementById('numeroTelefono').value);
+   var email = document.getElementById('email').value;
+   var contrasena = document.getElementById('password').value;
+   var rol = document.getElementById('rol').value;
+    data ={
         "idUsuario": 0,
         "numeroCedula": numeroCedula,
         "nombreCompleto": nombreCompleto,
-        "fechaNacimiento": "2023-06-05T00:00:00",
+        "fechaNacimiento": fechaNacimiento,
         "numeroTelefono": numeroTelefono,
         "email": email,
         "contrasena": contrasena,
         "rol": rol,
         "estado": "ACT"
-        };
-        console.log(data);
-        fetch("http://SistemaCovid-19.somee.com/Usuarios/agregar",
-            {
-                method: "PUT",
-                mode: 'cors',
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data)
-            })
-            .then(respuesta => respuesta.json())
-            .then((contenidoRespuesta) => {
-                console.log(contenidoRespuesta);
-            })
-            .catch(console.log)
-    });
+    };
+}
+//meto que maneja la interfaz de los usuarios el form
+(function () {
+    'use strict'
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
 
-function agregarUsuario(e) {
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                else {
+                    agregarUsuario(event);
+                    form.reset();
+                    setTimeout(cargarUsuario(),2500);
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+
+
+ function agregarUsuario(e) {
     e.preventDefault();
     cargarDatos();
     console.log(data);
@@ -71,9 +61,13 @@ function agregarUsuario(e) {
             console.log(contenidoRespuesta);
         })
         .catch(console.log)
+       
+        
 }
+
 function cargarUsuario() {
-    var tablaUsuario = document.querySelector('#tBody-Usuario');
+    tablaUsuario.innerHTML='';
+    console.log("hola cargando .....");
     fetch("http://SistemaCovid-19.somee.com/Usuarios")
         .then(respuesta => respuesta.json())
         .then((data) => {
@@ -100,22 +94,4 @@ function cargarUsuario() {
         });
 }
 
-(function () {
-    'use strict'
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                else {
-                }
-                form.classList.add('was-validated')
-            }, false)
-        })
-})()
+cargarUsuario();
